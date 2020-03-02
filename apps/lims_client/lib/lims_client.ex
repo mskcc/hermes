911c32d0fc6f649d4977638cfb70d3f7445114c3
Adrian Fraiha <aef@fastmail.com>
2020-03-02 14:25:16 -0500

    LIMS client

--- apps/lims_client/lib/lims_client.ex
+++ apps/lims_client/lib/lims_client.ex
@@ -1,18 +1,32 @@
 defmodule LimsClient do
+  use Tesla, only: ~w(get)a
+
+  plug(Tesla.Middleware.BaseUrl, Application.fetch_env!(:lims_client, :url))
+
+  plug(Tesla.Middleware.BasicAuth,
+    username: Application.fetch_env!(:lims_client, :username),
+    password: Application.fetch_env!(:lims_client, :password)
+  )
+
+  plug(Tesla.Middleware.DecodeJson)
+
   @moduledoc """
   Documentation for `LimsClient`.
   """
 
   @doc """
-  Hello world.
+  Get Sample Manifest
 
   ## Examples
 
-      iex> LimsClient.hello()
-      :world
+      iex> result = LimsClient.get_sample_manifest("1000")
+      ...> with {:ok, %Tesla.Env{}} <- result, do: :passed
 
   """
-  def hello do
-    :world
+  def get_sample_manifest(sample_id) when is_binary(sample_id),
+    do: get_sample_manifest([sample_id])
+
+  def get_sample_manifest(sample_id) when is_list(sample_id) do
+    get("/api/getSampleManifest")
   end
 end
