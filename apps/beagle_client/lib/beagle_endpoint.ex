0fe217b6f62d87801c30dca7b1a1de21da34c120
Nikhil <nikhilkumar516@gmail.com>
2020-11-10 12:58:58 -0500

    Added beagle_endpoint module

---
+++ apps/beagle_client/lib/beagle_endpoint.ex
@@ -0,0 +1,8 @@
+defmodule BeagleEndpoint do
+
+	def const_file_query, do: "/v0/fs/files/"
+	def const_file_groups, do: "/v0/fs/file-groups/"
+	def const_assay, do: "/v0/etl/assay"
+	def const_batch_patch_files, do: "/v0/fs/batch-patch-files"
+
+end
