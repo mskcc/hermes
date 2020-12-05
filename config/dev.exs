d2053bb5ce079ea4f5e5afab1315ba317647dd51
Nikhil <nikhilkumar516@gmail.com>
2020-12-04 23:34:19 -0500

    Don't truncate logs in dev mode

--- config/dev.exs
+++ config/dev.exs
@@ -108,6 +108,7 @@ config :voyager, VoyagerWeb.Endpoint,
 
 # Do not include metadata nor timestamps in development logs
 config :logger, :console, format: "[$level] $message\n"
+config :logger, truncate: :infinity
 
 # Set a higher stacktrace during development. Avoid configuring such
 # in production as building large stacktraces may be expensive.
