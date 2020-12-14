610c7275bbdbc2ccbaf5ca09bba31b9f879a518a
Nikhil <nikhilkumar516@gmail.com>
2020-12-14 16:46:14 -0500

    Some cleanup

--- mix.exs
+++ mix.exs
@@ -7,14 +7,6 @@ defmodule Seqosystem.MixProject do
       version: "0.1.0",
       start_permanent: Mix.env() == :prod,
       deps: deps()
-#      releases: [
-#        voyager: [
-#          include_executables_for: [:unix],
-#          applications: [
-#            voyager: :permanent
-#          ]
-#        ]
-#      ]
     ]
   end
 
