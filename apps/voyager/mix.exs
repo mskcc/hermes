f3f7cbb333129e5917ad66fb7a62b9e6a9d72ce1
Nikhil Kumar <nikhilkumar516@gmail.com>
2021-09-16 16:58:26 -0400

    Update mix.exs

--- apps/voyager/mix.exs
+++ apps/voyager/mix.exs
@@ -4,7 +4,7 @@ defmodule Voyager.MixProject do
   def project do
     [
       app: :voyager,
-      version: append_revision("0.2.1"),
+      version: append_revision("1.0.0"),
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
